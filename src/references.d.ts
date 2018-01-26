0a3ebd0737f3ce5ecdd0f5df2d13bb4e6ca40689
Jogboms <jeremiahogbomo@gmail.com>
2018-01-26 09:57:42 +0100

    chore: add typings for Paystack iOS & Android

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,2 +1,32 @@
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
+/// <reference path="./objc!Paystack.d.ts" />
+
+declare namespace co {
+    export namespace paystack {
+        export namespace android {
+            class Paystack {
+                static TransactionCallback(a);
+            }
+            class PaystackSdk {
+                static initialize(ApplicationContext: any);
+                static chargeCard(a, b, c);
+                static setPublicKey(a);
+            }
+            class Transaction {
+                getReference();
+            }
+            export namespace model {
+                class Card {
+                    static Builder(a, b, c, d): { build: Function };
+                }
+                class Charge {
+                    static Bearer: {
+                        subaccount,
+                        account
+                    };
+                }
+            }
+        }
+    }
+}
