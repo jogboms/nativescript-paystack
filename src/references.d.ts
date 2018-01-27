7b2ff4fa0b067841ffcbe648016a19172b09ad7d
Jogboms <jeremiahogbomo@gmail.com>
2018-01-27 16:25:04 +0100

    chore: improve native typings support

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,42 +1,4 @@
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
-/// <reference path="./objc!Paystack.d.ts" />
-
-declare module co {
-    export module paystack {
-        export module android {
-            class PaystackSdk {
-                static initialize(ApplicationContext: any);
-                static chargeCard(a, b, c);
-                static setPublicKey(a);
-            }
-            class Transaction {
-                getReference();
-            }
-            export module Paystack {
-                export class TransactionCallback {
-                    public constructor(implementation: {
-                        onSuccess: (transaction: Transaction) => void;
-                        beforeValidate: (transaction: Transaction) => void;
-                        onError: (error: any, transaction: Transaction) => void;
-                    })
-                    public onSuccess(transaction: Transaction): void;
-                    public beforeValidate(transaction: Transaction): void;
-                    public onError(error: any, transaction: Transaction): void;
-                }
-            }
-            export module model {
-                class Card {
-                    constructor(a, b, c, d);
-                    static Builder(a, b, c, d): { build: Function };
-                }
-                class Charge {
-                    static Bearer: {
-                        subaccount,
-                        account
-                    };
-                }
-            }
-        }
-    }
-}
+/// <reference path="./typings/objc!Paystack.d.ts" />
+/// <reference path="./typings/java!Paystack.d.ts" />
