a3abe0ed1275ab4470f898da3a2b7c4c069a99e4
Afees Lawal <moderateepheezy@gmail.com>
2018-01-27 14:53:43 +0100

    feat: android support

--- src/references.d.ts
+++ src/references.d.ts
@@ -2,12 +2,9 @@
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
 /// <reference path="./objc!Paystack.d.ts" />
 
-declare namespace co {
-    export namespace paystack {
-        export namespace android {
-            class Paystack {
-                static TransactionCallback(a);
-            }
+declare module co {
+    export module paystack {
+        export module android {
             class PaystackSdk {
                 static initialize(ApplicationContext: any);
                 static chargeCard(a, b, c);
@@ -16,8 +13,21 @@ declare namespace co {
             class Transaction {
                 getReference();
             }
-            export namespace model {
+            export module Paystack {
+                export class TransactionCallback {
+                    public constructor(implementation: {
+                        onSuccess: (transaction: Transaction) => void;
+                        beforeValidate: (transaction: Transaction) => void;
+                        onError: (error: any, transaction: Transaction) => void;
+                    })
+                    public onSuccess(transaction: Transaction): void;
+                    public beforeValidate(transaction: Transaction): void;
+                    public onError(error: any, transaction: Transaction): void;
+                }
+            }
+            export module model {
                 class Card {
+                    constructor(a, b, c, d);
                     static Builder(a, b, c, d): { build: Function };
                 }
                 class Charge {
