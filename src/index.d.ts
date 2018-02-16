bdb7d827b9f726ee7af360a44e8aab62e5dc2451
Jogboms <jeremiahogbomo@gmail.com>
2018-02-16 15:41:41 +0100

    chore: update typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,19 +1,21 @@
-import { Common, NSPaystackResponse } from "./paystack.common";
-export declare class NSPaystack extends Common {
-  private charge;
-  private transaction;
-  getPublicKey(): string;
-  initialize(publicKey: string): void;
-  setPublicKey(publicKey: string): void;
-  payment(params: {
-    amount: number;
-    email: string;
-    number: string;
-    cvc: string;
-    year: number;
-    month: number;
-  }): Promise<NSPaystackResponse>;
-  private chargeCard();
+import {
+    Common,
+    Payment,
+    NSPaymentParams,
+    NSPaystackResponse
+} from "./paystack.common";
+export declare class NSPayment implements Payment {
+    private _charge;
+    private _transaction;
+    constructor(params: NSPaymentParams);
+    addCustomField(name: string, value: string): this;
+    addMetadata(name: string, value: string): this;
+    charge(): Promise<NSPaystackResponse>;
 }
-
+export declare class NSPaystack extends Common {
+    getPublicKey(): string;
+    initialize(publicKey: string): void;
+    setPublicKey(publicKey: string): void;
+    payment(params: NSPaymentParams): Payment;
+}
 export * from "./paystack.common";
